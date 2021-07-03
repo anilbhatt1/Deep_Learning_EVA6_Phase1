283da33ad39efa40996e26b8373294364f27682b
Anil Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2021-07-03 15:51:52 +0530

    Modified test_loss, train_loss, utilities for S8

--- src/test_loss.py
+++ src/test_loss.py
@@ -61,3 +61,42 @@ class test_losses():
       self.stats(test_acc, 'test_acc')
 
       print(f'Test set: Epoch : {current_epoch}/{self.total_epochs} Average loss: {test_loss :.4f}, Test Accuracy: {test_acc}')
+
+  def s8_test(self, current_epoch, scheduler, optimizer, tb_writer):
+      self.model.eval()
+      test_loss, correct, count_wrong = 0, 0, 0
+      with torch.no_grad():
+          for data, target in self.test_loader:
+              data, target = data.to(self.device), target.to(self.device)
+              output = self.model(data)
+              test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+              pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+              correct += pred.eq(target.view_as(pred)).sum().item()
+
+              if current_epoch == (self.total_epochs - 1):
+                  compare = pred.eq(target.view_as(pred))
+                  misclass_idx = (compare == False).nonzero(as_tuple=True)[0].tolist()
+                  for i in misclass_idx:
+                      self.stats(data[i], 'mis_img')
+                      self.stats(pred[i].item(), 'mis_pred')
+                      self.stats(target[i].item(), 'mis_lbl')
+
+      test_loss /= len(self.test_loader.dataset)
+      self.stats(round(test_loss, 6), 'test_loss')
+      test_acc = round((100. * correct / len(self.test_loader.dataset)), 2)
+      self.stats(test_acc, 'test_acc')
+
+      tb_writer.add_scalar('Test loss', round(test_loss, 4), global_step=current_epoch)
+      tb_writer.add_scalar('Acc/Test', test_acc, global_step=current_epoch)
+
+      # 'scheduler.step' is required to update LR while using a scheduler like ReduceLROnPlateau. Here metric used is 'test_loss'
+      scheduler.step(test_loss)
+
+      lr = 0
+      if scheduler and not isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+          lr = scheduler.get_last_lr()[0]  # Won't work for ReduceLROnPlateau
+      else:
+          lr = optimizer.param_groups[0]['lr']
+
+      print(
+          f'Test set: Epoch : {current_epoch}/{self.total_epochs} Average loss: {test_loss :.4f}, Test Accuracy: {test_acc}')
--- src/utilities.py
+++ src/utilities.py
@@ -1,5 +1,31 @@
 import numpy as np
+import cv2
+import torch
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
+import albumentations as A
+
+cuda = torch.cuda.is_available()
+
+class Alb_trans:
+    """
+    Class to create test and train transforms using Albumentations.
+    """
+
+    def __init__(self, transforms_list=[]):
+        self.transforms = A.Compose(transforms_list)
+
+    """
+    Library works only with named arguments and numpy array. Hence to make it compatible with torchvision, using wrapper to convert to np array
+    """
+
+    def __call__(self, img):
+        img = np.array(img)
+        img = self.transforms(image=img)['image']  # if ['image'] is not given, then it will give key error in dataloader
+        img = np.transpose(img, (2, 0, 1))
+        return img
 
 class stats_collector():
     def __init__(self):
@@ -50,28 +76,109 @@ class unnorm_img():
             img = img.permute(1, 2, 0)
         return img
 
-class plots():
+class cifar10_plots():
 
-    # 5 * 5 images
-    def plot_misclassified(self, test_stats, num_images, title, class_names, fig_size, unnorm_mispred):
+    def __init__(self, img_save_path, tb_writer):
+        self.channels_mean = [0.49139968, 0.48215841, 0.44653091]
+        self.channels_stdev = [0.24703223, 0.24348513, 0.26158784]
+        self.img_save_path  = img_save_path
+        self.tb_writer      = tb_writer
 
+    def unnormalize_cifar10(self, img):
+        for i in range(img.shape[0]):
+            img[i] = (img[i] * self.channels_stdev[i]) + self.channels_mean[i]
+        img = img.permute(1, 2, 0)
+        return img
+
+    def plot_cifar10_train_imgs(self, trainloader):
+
+        images, labels = next(iter(trainloader))
+        print(f'images.size() : {images.size()}, labels.size() : {labels.size()}')
+        num_classes = 10
+        num_images  = 5
+        fig_name = 'Train_Images'
+        lst = []
+        for i in range(num_classes):
+            idx = np.random.choice(np.where(labels[:] == i)[0], num_images)
+            lst.extend(images[idx])
+        disp_grid  = torchvision.utils.make_grid(lst, 5)
+        disp_grid1 = self.unnormalize_CIFAR(disp_grid)
+        self.tb_writer.add_image('Train-Images', disp_grid, 0)
+
+        plt.figure(figsize=(14, 14))
+        plt.imshow(disp_grid1)
+        plt.axis('off')
+        plt.savefig(f'{self.img_save_path}{fig_name}.jpg')
+
+    def plot_cifar10_gradcam_imgs(self, gcam_layers, images, labels, target_layers, predicted, disp_range):
+
+        fig_name    = 'Gradcam_Imgs_'
+        class_names = ['plane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        image_size  = (3, 32, 32)
+        a, b = disp_range
+        c = 6                            # 6 columns - 1st : Display layer names, 2nd to 6th : To show 5 images
+        r = len(target_layers) + 2       # 6 rows    - 1st : Pred Vs act, 2nd: Input, 3rd: layer1, 4th: layer2, 5th: layer3, 6th: layer4
+        fig = plt.figure(figsize=(16, 12))
+        fig.subplots_adjust(hspace=0.01, wspace=0.01)
+        ax = plt.subplot(r, c, 1)        # (6 rows, 5 columns, Index = 1)
+        ax.text(0.3, -0.5, f"Input \nImg {a + 1}-{b}", fontsize=14)
+        plt.axis('off')
+        for i in range(len(target_layers)):
+            target_layer = target_layers[i]
+            ax = plt.subplot(r, c, c * (i + 1) + 1)
+            ax.text(0.3, -0.5, target_layer, fontsize=14)
+            plt.axis('off')
+
+            for j in range(0, 5):
+                k = j + a
+                img_ = images[k].cpu()
+                img = np.uint8(255 * self.unnormalize_CIFAR(img_.view(image_size)))
+                if i == 0:
+                    ax = plt.subplot(r, c, j + 2)
+                    ax.text(0.1, 0.2, f"pred={class_names[predicted[k][0]]}\nactual={class_names[labels[k]]}",fontsize=10)
+                    plt.axis('off')
+                    plt.subplot(r, c, c + j + 2)
+                    plt.imshow(img, interpolation='bilinear')
+                    plt.axis('off')
+
+                heatmap = gcam_layers[i][k].cpu().numpy()[0]
+                heatmap = np.uint8(255 * heatmap)
+                heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+                heatmap = heatmap[:, :, ::-1]
+                superimposed_img = cv2.resize(cv2.addWeighted(img, 0.5, heatmap, 0.5, 0), (32, 32))
+                plt.subplot(r, c, (i + 2) * c + j + 2)
+                plt.imshow(superimposed_img, interpolation='bilinear')
+                plt.axis('off')
+        plt.savefig(f'{self.img_save_path}{fig_name}{a}_{b}.jpg')
+        img_arr = plt.imread(f'{self.img_save_path}Gradcam_Imgs_{a}_{b}.jpg')
+        img_arr = img_arr.transpose(2, 0, 1)
+        self.tb_writer.add_image(f'GradCam-Images/Misclassified_Imgs_{a}_{b}', img_arr, 0)
+        plt.show()
+
+    def plot_cifar10_misclassified(self, counters, num_images):
+
+        fig_name = 'Test_Misclass_Imgs'
+        fig_size = (12, 12)
         figure = plt.figure(figsize=fig_size)
-        print(f'** Plotting misclassified images from last epoch for {title} **')
+        print(f'** Plotting misclassified test images from last epoch for CIFAR-10 **')
         print('\n')
-        class_names_dict = class_names
-        if len(test_stats.img) > num_images:
+        class_names_dict = {0:'plane', 1:'automob', 2:'bird', 3:'cat',4:'deer', 5:'dog', 6:'frog', 7:'horse', 8:'ship', 9:'truck'}
+        if len(counters['mis_img']) > num_images:
             for i in range(num_images):
                 plt.subplot(5, 5, i + 1)
                 plt.axis(False)
-                unnorm = unnorm_mispred.unnorm_gray(test_stats.img[i].squeeze().cpu())
-                plt.imshow(unnorm, cmap='gray_r')
-                prediction = class_names_dict.get(test_stats.pred[i])
-                actual = class_names_dict.get(test_stats.label[i])
+                unnorm_img = self.unnormalize(counters['mis_img'][i].cpu())
+                plt.imshow(unnorm_img, interpolation='none')
+                prediction = class_names_dict.get(counters['mis_pred'][i])
+                actual     = class_names_dict.get(counters['mis_lbl'][i])
                 s = "pred=" + str(prediction) + " act=" + str(actual)
                 plt.text(2, -1, s)
+            plt.savefig(f'{self.img_save_path}{fig_name}.jpg')
+            mis_arr = plt.imread(f'{self.img_save_path}{fig_name}.jpg')
+            mis_arr = mis_arr.transpose(2, 0, 1)
+            self.tb_writer.add_image(f'Test-Misclassified_Imgs', mis_arr, 0)
         else:
-            print(f'Unable to plot - Less than {num_images} images, only have {len(test_stats.img)} images')
-
+            print(f'Unable to plot - Less than {num_images} images')
 
 counters = {'train_loss':[], 'train_acc':[], 'test_loss':[], 'test_acc':[], 'mis_img':[], 'mis_pred':[], 'mis_lbl':[]}
 
@@ -80,3 +187,152 @@ def ctr():
         counters[type].append(value)
         return counters
     return inner
+
+def CIFAR10_data_prep():
+    if cuda:
+        torch.cuda.manual_seed(1)
+
+    dataloader_args = dict(shuffle=True, batch_size=128, num_workers=2, pin_memory=True) if cuda else dict(shuffle=True,
+                                                                                                           batch_size=64)
+
+    channels_mean  = [0.49139968, 0.48215841, 0.44653091]
+    channels_stdev = [0.24703223, 0.24348513, 0.26158784]
+
+    test_transforms  = Alb_trans([A.Normalize(mean=channels_mean, std=channels_stdev), ])
+    train_transforms = Alb_trans([A.Rotate((-5, 5)),
+                                  A.RandomCrop(32, 32),
+                                  A.Normalize(mean=channels_mean, std=channels_stdev),
+                                  A.Sequential([A.PadIfNeeded(min_height=40, min_width=40,
+                                                              border_mode=cv2.BORDER_CONSTANT,
+                                                              value=(0.49139968, 0.48215841, 0.44653091)),
+                                                A.RandomCrop(32, 32)], p=1.0),
+                                  A.Cutout(num_holes=1, max_h_size=16, max_w_size=16),
+                                  ])
+
+    train_data = datasets.CIFAR10('./data', train=True, download=True, transform=train_transforms)
+    test_data  = datasets.CIFAR10('./data', train=False, download=True, transform=test_transforms)
+
+    # train dataloader
+    trainloader = torch.utils.data.DataLoader(train_data, **dataloader_args)
+
+    # test dataloader
+    testloader = torch.utils.data.DataLoader(test_data, **dataloader_args)
+
+    return trainloader, testloader
+
+def create_tensorboard_writer(*args):
+    if args is not None:
+        tb_events_path = args[0]
+        tb_writer = SummaryWriter(logdir=tb_events_path)
+    else:
+        tb_writer = SummaryWriter()
+    return tb_writer
+
+class GradCAM:
+    """ Class for extracting activations and
+    registering gradients from targetted intermediate layers
+    target_layers = list of convolution layer index as shown in summary
+    """
+    def __init__(self, model, candidate_layers=None):
+        def save_fmaps(key):
+          def forward_hook(module, input, output):
+              self.fmap_pool[key] = output.detach()
+
+          return forward_hook
+
+        def save_grads(key):
+          def backward_hook(module, grad_in, grad_out):
+              self.grad_pool[key] = grad_out[0].detach()
+
+          return backward_hook
+
+        self.device = next(model.parameters()).device
+        self.model = model
+        self.handlers = []  # a set of hook function handlers
+        self.fmap_pool = {}
+        self.grad_pool = {}
+        self.candidate_layers = candidate_layers  # list
+
+        for name, module in self.model.named_modules():
+            if self.candidate_layers is None or name in self.candidate_layers:
+                self.handlers.append(module.register_forward_hook(save_fmaps(name)))
+                self.handlers.append(module.register_backward_hook(save_grads(name)))
+
+    def _encode_one_hot(self, ids):
+        one_hot = torch.zeros_like(self.nll).to(self.device)
+        one_hot.scatter_(1, ids, 1.0) # Refer https://yuyangyy.medium.com/understand-torch-scatter-b0fd6275331c to understand torch.scatter_
+        return one_hot
+
+    def forward(self, image):
+        self.image_shape = image.shape[2:] # HxW
+        self.nll = self.model(image)       # This will be a tensor have probabilties
+        return self.nll.sort(dim=1, descending=True)  # ordered results, returns (values, indices)
+
+    def backward(self, ids):
+        """
+        Class-specific backpropagation
+        """
+        one_hot = self._encode_one_hot(ids)
+        self.model.zero_grad()
+        self.nll.backward(gradient=one_hot, retain_graph=True)
+
+    def remove_hook(self):
+        """
+        Remove all the forward/backward hook functions
+        """
+        for handle in self.handlers:
+            handle.remove()
+
+    def _find(self, pool, target_layer):
+        if target_layer in pool.keys():
+            return pool[target_layer]
+        else:
+            raise ValueError("Invalid layer name: {}".format(target_layer))
+
+    def generate(self, target_layer):
+        fmaps = self._find(self.fmap_pool, target_layer)
+        grads = self._find(self.grad_pool, target_layer)
+        weights = F.adaptive_avg_pool2d(grads, 1)
+
+        gcam = torch.mul(fmaps, weights).sum(dim=1, keepdim=True)
+        gcam = F.relu(gcam)
+        # need to capture image size during forward pass
+        gcam = F.interpolate(
+            gcam, self.image_shape, mode="bilinear", align_corners=False
+        )
+
+        # scale output between 0,1
+        B, C, H, W = gcam.shape
+        gcam = gcam.view(B, -1)
+        gcam -= gcam.min(dim=1, keepdim=True)[0]
+        gcam /= gcam.max(dim=1, keepdim=True)[0]
+        gcam = gcam.view(B, C, H, W)
+
+        return gcam
+
+def GRADCAM(images, labels, model, target_layers):
+    model.eval()
+    # map input to device
+    images = torch.stack(images).to(device)
+    # set up grad cam
+    gcam = GradCAM(model, target_layers)
+    # forward pass
+    probs, ids = gcam.forward(images)
+    # outputs against which to compute gradients
+    ids_ = torch.LongTensor(labels).view(len(images),-1).to(device)
+    # backward pass
+    gcam.backward(ids=ids_)
+    layers = []
+    for i in range(len(target_layers)):
+        target_layer = target_layers[i]
+        print("Generating Grad-CAM @{}".format(target_layer))
+        # Grad-CAM
+        layers.append(gcam.generate(target_layer=target_layer))
+    # remove hooks when done
+    gcam.remove_hook()
+    return layers, probs, ids
+
+
+
+
+
